dcdb3b55fc7319db33c6ca97c5ca1c88d235a8fb
Crowdbotics <team@crowdbotics.com>
2022-07-08 10:20:34 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -1,1 +1,16 @@
+from django.conf import settings
 from django.db import models
+
+
+class User1(models.Model):
+    "Generated Model"
+    name = models.CharField(
+        max_length=200,
+    )
+    email = models.EmailField(
+        max_length=254,
+    )
+    password = models.CharField(
+        max_length=200,
+    )
+    age = models.BigIntegerField()
